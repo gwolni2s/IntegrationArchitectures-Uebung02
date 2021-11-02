010af83854e90c8c4c9db0d27af81de7b9866060
Georg Wolnik <92213086+gwolni2s@users.noreply.github.com>
2021-11-02 18:58:14 +0100

    Create Repo Uebung02

---
+++ src/main/java/org/example/IntegrationArchitecturesUebung02/IntegrationArchitecturesUebung02Application.java
@@ -0,0 +1,13 @@
+package org.example.IntegrationArchitecturesUebung02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationArchitecturesUebung02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationArchitecturesUebung02Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/IntegrationArchitecturesUebung02/IntegrationArchitecturesUebung02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.IntegrationArchitecturesUebung02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationArchitecturesUebung02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
